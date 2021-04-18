788ecc55492d21343ffcf50774aea4f30adbb2ae
ngvutrkien <theory1195@gmail.com>
2021-04-19 00:11:17 +0700

    ! Re-align dots.

--- src/game/Dots.js
+++ src/game/Dots.js
@@ -8,11 +8,12 @@ class Dots extends PIXI.Container
     InitDots(name)
     {
         let dots = DataDefine.dots[name];
+        let dotSize = 30;
         dots.forEach(dot =>
         {
             let rect = new PIXI.Graphics()
                 .beginFill(0xFF0000)
-                .drawRect(APP.GetWidth() / 2 + dot.x, APP.GetHeight() / 2 + dot.y, 15, 15)
+                .drawRect(APP.GetWidth() / 2 + dot.x - dotSize / 2, APP.GetHeight() / 2 + dot.y - dotSize / 2, dotSize, dotSize)
                 .endFill();
             this.addChild(rect);
         });
