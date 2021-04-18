4de56226e0f0f9a2a0eafb6bc75304dd3c4e072c
Nguyen Vu Trung Kien <theory1195@gmail.com>
2021-04-19 06:34:03 +0700

    + Add individual Dot.

---
+++ src/game/Dot.js
@@ -0,0 +1,27 @@
+class Dot extends PIXI.Container
+{
+    constructor(dot, dotSize)
+    {
+        super();
+
+        this.rect = new PIXI.Graphics()
+            .beginFill(0xFF0000)
+            .drawRect(0, 0, dotSize, dotSize)
+            .endFill();
+        this.rect.position.set(APP.GetWidth() / 2 + dot.x - dotSize / 2, APP.GetHeight() / 2 + dot.y - dotSize / 2);
+        this.addChild(this.rect);
+    }
+
+    GetRect()
+    {
+        let rect = {
+            x: this.rect.x,
+            y: this.rect.y,
+            width: this.rect.width,
+            height: this.rect.height
+        };
+        return rect;
+    }
+};
+
+module.exports = Dot;
--- src/game/Dots.js
+++ src/game/Dots.js
@@ -1,3 +1,5 @@
+const Dot = require('./Dot');
+
 class Dots extends PIXI.Container
 {
     constructor()
@@ -11,12 +13,8 @@ class Dots extends PIXI.Container
         let dotSize = 30;
         dots.forEach(dot =>
         {
-            let rect = new PIXI.Graphics()
-                .beginFill(0xFF0000)
-                .drawRect(0, 0, dotSize, dotSize)
-                .endFill();
-            rect.position.set(APP.GetWidth() / 2 + dot.x - dotSize / 2, APP.GetHeight() / 2 + dot.y - dotSize / 2);
-            this.addChild(rect);
+            let node = new Dot(dot, dotSize);
+            this.addChild(node);
         });
     }
 
@@ -29,13 +27,7 @@ class Dots extends PIXI.Container
 
         if (Input.IsTouchMove(event))
         {
-            let child = this.children[0];
-            let rect = {
-                x: child.x,
-                y: child.y,
-                width: child.width,
-                height: child.height
-            }
+            let rect = this.children[0].GetRect();
             console.log(Utils.CollisionPointRect(touch.x, touch.y, rect));
         }
         else if (Input.IsTouchUp(event))
